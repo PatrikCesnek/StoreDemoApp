96c11e6ac374e03937df96f80d67cc1603c3317b
Patrik Cesnek <pcesnek290@gmail.com>
2024-11-23 12:57:24 +0100

    Initial Commit

---
+++ StoreAppDemo/StoreAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreAppDemoApp.swift
+//  StoreAppDemo
+//
+//  Created by Patrik Cesnek on 23/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct StoreAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
